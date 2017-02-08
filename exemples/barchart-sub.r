9b9b636a7e1863777569cf7a9e5c20ba152fda53
migueldsw <mdsw@cin.ufpe.br>
2017-02-08 12:12:43 -0300

    barchart plot exemple update

--- exemples/barchart-sub.r
+++ exemples/barchart-sub.r
@@ -1,4 +1,6 @@
+#
 #Ref.: https://www.r-bloggers.com/building-barplots-with-error-bars/
+#Ref.2: http://stackoverflow.com/questions/29768219/grouped-barplot-in-r-with-error-bars
 myData <- aggregate(mtcars$mpg,
     by = list(cyl = mtcars$cyl, gears = mtcars$gear),
     FUN = function(x) c(mean = mean(x), sd = sd(x),
@@ -15,13 +17,26 @@ myData$names <- c(paste(myData$cyl, "cyl /",
 
 
 #Barplots using ggplot2
-#Grouped barplots
-
 #install ggplot2
 #Ref. (if fails): http://stackoverflow.com/questions/30636670/ggplot2-fails-to-install-on-r-3-0-2 
 #install.packages("ggplot2")
 
 library(ggplot2)
+
+#Simple barplot
+dodge <- position_dodge(width = 0.9)
+limits <- aes(ymax = myData$mean + myData$se,
+              ymin = myData$mean - myData$se)
+
+p <- ggplot(data = myData, aes(x = names, y = mean, fill = names))
+
+p + geom_bar(stat = "identity", position = dodge) +
+  geom_errorbar(limits, position = dodge, width = 0.25) +
+  theme(axis.text.x=element_blank(), axis.ticks.x=element_blank(),
+        axis.title.x=element_blank())
+
+#Grouped barplots
+
 
 
 limits <- aes(ymax = myData$mean + myData$se,
